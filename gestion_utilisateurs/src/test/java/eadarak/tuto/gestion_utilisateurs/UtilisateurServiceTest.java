de5fe9530d8854aa2b04aa841df589a04621244a
eadarak00 <eadarak2001@gmail.com>
2025-01-08 08:55:26 +0000

    implementation des tests unitaires de la logique metier du classe utilisateur

--- gestion_utilisateurs/src/test/java/eadarak/tuto/gestion_utilisateurs/UtilisateurServiceTest.java
+++ gestion_utilisateurs/src/test/java/eadarak/tuto/gestion_utilisateurs/UtilisateurServiceTest.java
@@ -10,10 +10,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+// import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+// import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
